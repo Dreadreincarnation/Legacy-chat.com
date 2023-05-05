ad3a3f502e0d32f8e75836e53377336eb8f48354
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-05 12:18:03 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const KVdb = require('kvdb.io')
+const bucket = KVdb.bucket('2QSYWhBjfeTBDvNbuj4dFz', 'gjgdovxgtoutevbif')
 const lesgo = document.querySelector('body')
 const divContainer = document.querySelector('#titles')
 const leso = document.querySelector('#mainsr')
