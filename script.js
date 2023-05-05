6efe8d9e3151ed1c0da242a536d63b9d06e60093
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-04 22:22:00 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const lesgo = document.querySelector('')
 const divContainer = document.querySelector('')
 const leso = document.querySelector('')
-const ledo
-const cerl
-const zieko
-const lias
+const ledo = document.querySelector('')
+const cerl = document.querySelector('')
+const zieko = document.querySelector('')
+const lias = document.querySelector('')
 const azra
 const coro
 const cova
