426e44f526488b3e926d52b08f4d902461480911
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-05 06:30:26 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const coro = document.querySelector('#loc')
 const cova = document.querySelector('#gen')
 const mare = document.querySelector('#gentxt')
 const nala = document.querySelector('#createbtn')
+const KVdb = require('kvdb.io')
 
 let Create = function(){
   leso.style.display = 'none';
