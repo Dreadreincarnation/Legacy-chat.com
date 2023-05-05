6cb7dc99efc5f243e7ddecbdf1b8ed94945da0fe
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-05 19:54:46 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global.KVdb = factory())
 }(this, (function() { 'user strict';
 const fetch = typeof this.window !== 'undefined' ? window.fetch : require('node-fetch');
+let KVdb
 const lesgo = document.querySelector('body')
 const divContainer = document.querySelector('#titles')
 const leso = document.querySelector('#mainsr')
