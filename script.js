13c359c025cc2cf0d1dbb98ffef4d4c3b5a3ff49
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-05 04:42:54 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const coro = document.querySelector('#loc')
 const cova = document.querySelector('#gen')
 const mare = document.querySelector('#gentxt')
 const nala = document.querySelector('#createbtn')
-
+import kvbd
 let Create = function(){
   leso.style.display = 'none';
   zieko.style.display = 'block';
