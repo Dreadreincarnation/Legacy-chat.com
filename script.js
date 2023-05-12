2beb7be3f8dc0625ed6943925b70f81e30b1e4d5
Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>
2023-05-12 17:57:13 -0400

    Update script.js
    Signed-off-by: Dreadreincarnation <132242461+Dreadreincarnation@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const coro = document.querySelector('#loc')
 const cova = document.querySelector('#gen')
 const mare = document.querySelector('#gentxt')
 const nala = document.querySelector('#createbtn');
-import { Octokit, App } from "https://cdn.skypack.dev/octokit";
+import { Octokit, App } from 'https://cdn.skypack.dev/octokit';
 const octokit = new Octokit({
   auth: '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'
 });
